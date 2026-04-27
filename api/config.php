93c656bf7b8b120746877b7c76fb61f9c9c1b65f
Axel <axelguzman996@gmail.com>
2026-04-27 14:44:10 -0600

    Fix DB port config

--- api/config.php
+++ api/config.php
@@ -1,9 +1,10 @@
 <?php
 return [
     'LOG_PATH' => __DIR__ . '/Log',
-    'DB_USERNAME' => 'root',
-    'DB_PASSWORD' => '',
-    'DB_HOST' => 'localhost',
-    'DB_DBNAME' => 'subasta_relojes',
-    'SECRET_KEY'=>'e0d17975bc9bd57eee132eecb6da6f11048e8a88506cc3bffc7249078cf2a77a'
-];  
+    'DB_USERNAME' => getenv('DB_USERNAME'),
+    'DB_PASSWORD' => getenv('DB_PASSWORD'),
+    'DB_HOST'     => getenv('DB_HOST'),
+    'DB_PORT'     => getenv('DB_PORT') ?: '3306',
+    'DB_DBNAME'   => getenv('DB_DBNAME'),
+    'SECRET_KEY'  => 'e0d17975bc9bd57eee132eecb6da6f11048e8a88506cc3bffc7249078cf2a77a'
+];
